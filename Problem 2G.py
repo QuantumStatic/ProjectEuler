18afa93c6b8e8d349ff208b5260a149540d016d7
Utkarsh Jain <utkarshworkemail@gmail.com>
2020-07-13 13:31:07 +0530

    Update Problem 2G.py

--- Problem 2G.py
+++ Problem 2G.py
@@ -9,7 +9,7 @@ def Problem_2():
         return floor(pow((1 + sqrt(5)) / 2, n) / sqrt(5) + 0.5)
 
     num, Answer = 1, int()
-    while (n: = fib(num)) < 4000000:
+    while (n:= fib(num)) < 4000000:
         if n % 2 == 0:
             Answer += n
         num += 1
