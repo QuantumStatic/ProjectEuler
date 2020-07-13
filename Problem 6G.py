caf69d34cc20d0bf4e9c5d3c90e786aa70332328
Utkarsh Jain <utkarshworkemail@gmail.com>
2020-07-13 20:00:30 +0530

    Update Problem 6G.py

--- Problem 6G.py
+++ Problem 6G.py
@@ -1,7 +1,7 @@
-from myFunctionsG import execute_it
+from myFunctionsG import execute_this
 
 
-@execute_it
+@execute_this
 def Problem_6():
     num = 100
     print(abs((num*(num+1)*(2*num+1)//6) - (pow(num*(num+1)//2, 2))))
