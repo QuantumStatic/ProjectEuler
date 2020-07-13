828089036de48bda22b2888d0b8546f72f24d8f8
Utkarsh Jain <utkarshworkemail@gmail.com>
2020-07-13 13:10:50 +0530

    Create Problem 1G.py

---
+++ Problem 1G.py
@@ -0,0 +1,39 @@
+# Improvemnts suggested by @aarnavg17
+from myFunctionsG import execute_it
+
+
+@execute_it
+def Problem_1():
+    FinalSum, endPoint, firstNum, lastNum = int(), 999, 3, int()
+
+    temp = endPoint
+    while temp:
+        if temp % firstNum == 0:
+            lastNum = temp
+            break
+        temp -= 1
+    else:
+        lastNum = 0
+    FinalSum += lastNum * (firstNum + lastNum) // (2 * firstNum)
+
+    firstNum, temp = 5, endPoint
+    while temp:
+        if temp % firstNum == 0:
+            lastNum = temp
+            break
+        temp -= 1
+    else:
+        lastNum = 0
+    FinalSum += lastNum * (firstNum + lastNum) // (2 * firstNum)
+
+    firstNum, temp = 15, endPoint
+    while temp:
+        if temp % firstNum == 0:
+            lastNum = temp
+            break
+        temp -= 1
+    else:
+        lastNum = 0
+    FinalSum -= lastNum * (firstNum + lastNum) // (2 * firstNum)
+
+    print(FinalSum)
