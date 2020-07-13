74eaba37b8f2226c7a7162336160fe24b40356e9
Utkarsh Jain <utkarshworkemail@gmail.com>
2020-07-13 20:00:40 +0530

    Update Problem 3G.py

--- Problem 3G.py
+++ Problem 3G.py
@@ -1,8 +1,8 @@
-from myFunctionsG import execute_it, prime_checker as isprime
+from myFunctionsG import execute_this, prime_checker as isprime
 from math import sqrt, ceil
 
 
-@execute_it
+@execute_this
 def Problem_3():
 
     initNum = ceil(sqrt(600851475143))
