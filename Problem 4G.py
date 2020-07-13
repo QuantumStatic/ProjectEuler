b7e4304ff5fccaaf81946e83ad54898e986accde
Utkarsh Jain <utkarshworkemail@gmail.com>
2020-07-13 13:55:16 +0530

    Create Problem 4G.py

---
+++ Problem 4G.py
@@ -0,0 +1,16 @@
+from myFunctionsG import execute_it
+
+# if there are two numbers with n digits & m digits then their product can't have more than n+m digits
+
+
+@execute_it
+def Problem_4():
+    palindrome = int()
+
+    for x in range(999, 316, -1):
+        for y in range(x-1, 316, -1):
+            num = str(x * y)
+            if (num[0] == num[-1]) and (num[1] == num[-2]) and (num[2] == num[-3]) and int(num) > palindrome:
+                palindrome = int(num)
+
+    print(palindrome)
