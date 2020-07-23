628a6262e36e3fe145f73226b39f86e9f03a5eaf
Utkarsh Jain <utkarshworkemail@gmail.com>
2020-07-23 09:47:56 +0530

    Create Problem 22G.py

---
+++ Problem 22G.py
@@ -0,0 +1,17 @@
+from myFunctionsG import execute_this
+
+
+@execute_this
+def main():
+    with open("Problem_22.txt") as file:
+        name = file.read().split(',')
+    final_sum = int()
+    for x in range(len(name)):
+        name[x] = name[x][1:len(name[x])-1]
+    name.sort()
+    for x in range(len(name)):
+        curr_sum = int()
+        for y in name[x]:
+            curr_sum += ord(y)-64
+        final_sum += curr_sum*(x+1)
+    print(final_sum)
