176925c450d5f8d24e249284636e4e64d6cd63c2
Utkarsh Jain <utkarshworkemail@gmail.com>
2020-08-02 22:54:32 +0530

    Create Problem 25G.py

---
+++ Problem 25G.py
@@ -0,0 +1,20 @@
+from math import sqrt, floor
+from myFunctionsG import execute_this
+
+
+@execute_this
+def Problem_25():
+
+    register, number = {0: 0, 1: 1, 2: 1}, 1
+
+    def fib(n):
+        try:
+            return register[n]
+        except:
+            register[n] = fib(n-1)+fib(n-2)
+        return register[n]
+
+    while len(str(fib(number))) < 1000:
+        number += 1
+
+    print(number)
