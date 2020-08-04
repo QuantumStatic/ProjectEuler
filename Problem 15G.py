679d03fb727a0c8eebfa8f20647d27e2c70f5013
Utkarsh Jain <utkarshworkemail@gmail.com>
2020-08-04 18:15:05 +0530

    Update Problem 15G.py

--- Problem 15G.py
+++ Problem 15G.py
@@ -1,5 +1,6 @@
 # Question can be rephrased as in how many ways can I go right. If I go right, I would have to go down at some point. thinkin how many ways I can go down is the same thought process.
 # Credits to @AbhimanyuBhati, for thinking in this direction before I did.
+# Runtime - 1.5 microseconds (0.0000015 seconds)
 from myFunctionsG import execute_this
 from math import comb
 
