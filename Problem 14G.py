5f0dba99b7affbcf96383eb75c3d8015ce31cd27
Utkarsh Jain <utkarshworkemail@gmail.com>
2020-08-04 18:13:25 +0530

    Update Problem 14G.py

--- Problem 14G.py
+++ Problem 14G.py
@@ -1,4 +1,5 @@
 # previous version wasn't that fast. After discussion with @aarnavg17, decided to use a dictionary
+# Runtime - 1 Second 400 milliseconds (1.4 seconds)
 from myFunctionsG import execute_this
 
 
@@ -10,12 +11,12 @@ def Problem_14():
         try:
             return (register[num])
         except:
-            register[num] = (2 + collatz((3*num + 1) //2)) if num % 2 else (1 + collatz(num // 2))
+            register[num] = (2 + collatz((3*num + 1) // 2)) if num % 2 else (1 + collatz(num // 2))
             return (register[num])
 
     longestChain, biggestNum = int(), int()
     for n in range(3, 1_000_000):
-        if (currChain:=collatz(n)) > longestChain:
+        if (currChain := collatz(n)) > longestChain:
             longestChain = currChain
             biggestNum = n
 
