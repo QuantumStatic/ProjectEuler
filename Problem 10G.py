89ae8f8a6dd8746ba035e3fc1c6b3b5cb0abb765
Utkarsh Jain <utkarshworkemail@gmail.com>
2020-08-04 18:01:46 +0530

    Update Problem 10G.py

--- Problem 10G.py
+++ Problem 10G.py
@@ -1,6 +1,7 @@
+# Runtime - 215 milliseconds (0.215 seconds)
 from myFunctionsG import sieveEratoAlt, execute_this
 
 
 @execute_this
 def Problem_10():
-    print(sum(sieveEratoAlt(2_000_000))+2)
+    print(sum(sieveEratoAlt(2000000))+2)
