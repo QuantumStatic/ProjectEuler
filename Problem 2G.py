e17cb521b40e55b29c3f0ec035ab743f371b6354
Utkarsh Jain <utkarshworkemail@gmail.com>
2020-08-04 17:32:16 +0530

    Update Problem 2G.py

--- Problem 2G.py
+++ Problem 2G.py
@@ -1,3 +1,4 @@
+# Runtime - 4.2 microseconds (0.0000042 seconds)
 from myFunctionsG import execute_this
 from math import sqrt, floor
 
@@ -9,7 +10,7 @@ def Problem_2():
         return floor(pow((1 + sqrt(5)) / 2, n) / sqrt(5) + 0.5)
 
     num, Answer = 1, int()
-    while (n:= fib(num)) < 4000000:
+    while (n := fib(num)) < 4000000:
         if n % 2 == 0:
             Answer += n
         num += 1
